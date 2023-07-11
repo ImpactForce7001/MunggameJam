6804705e3fc8e5055ebb29b6f4966778dfa12bf8
AIE\guest050 <jwpolky5@gmail.com>
2023-07-11 15:09:08 +0930

    change scenes issue

--- Game Jam Mung/Assets/Scripts/UI/ChangeScene.cs
+++ Game Jam Mung/Assets/Scripts/UI/ChangeScene.cs
@@ -7,9 +7,20 @@ public class ChangeScene : MonoBehaviour
 {
     public void StartGame()
     {
-
+        GameManager.days = 0;
+        GameManager.timeElapsed = 0;
+        PlayerObjectInteraction.teeth = true;
+        PlayerObjectInteraction.eat = true;
+        PlayerObjectInteraction.wash = true;
+        PlayerObjectInteraction.reading = true;
+        PlayerObjectInteraction.JerryEat = true;
+        PlayerObjectInteraction.JerryWash = true;
+        PhoneController.strikeBook = false;
+        PhoneController.strikeFood = false;
+        PhoneController.strikeTeeth = false;
+        PhoneController.strikeLaundry = false;
         SceneManager.LoadScene(5);
-
+        
     }
     public void ExitGame()
     {
