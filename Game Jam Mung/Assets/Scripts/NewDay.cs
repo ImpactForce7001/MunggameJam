73073d841f8ead19632fa1535ad5afb782e5b16a
AIE\guest050 <jwpolky5@gmail.com>
2023-07-11 12:35:20 +0930

    made progress on new day script, included bools for player and jerry max points. Added game manager to louge room

--- Game Jam Mung/Assets/Scripts/NewDay.cs
+++ Game Jam Mung/Assets/Scripts/NewDay.cs
@@ -5,7 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class NewDay : MonoBehaviour
 {
-
+    public static int days = 0;
+    public static bool JerryWin = false;
+    public static bool PlayerWin = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +19,29 @@ public class NewDay : MonoBehaviour
     {
         if (JerryAi.timeUp)
         {
-            PlayerObjectInteraction.teeth = true;
-            PlayerObjectInteraction.eat = true;
-            PlayerObjectInteraction.wash = true;
-            PlayerObjectInteraction.reading = true;
-            SceneManager.LoadScene(2);
-            JerryAi.timeUp = false;
+            
+        }
+    }
+    public static void FinishDay()
+    {
+        PlayerObjectInteraction.teeth = true;
+        PlayerObjectInteraction.eat = true;
+        PlayerObjectInteraction.wash = true;
+        PlayerObjectInteraction.reading = true;
+        SceneManager.LoadScene(3);
+        GameManager.timeElapsed = 0;
+        days += 1;
+        if(days >= 4)
+        {
+            //load end scene
+            if(PlayerObjectInteraction.PlayerPoints == 16)
+            {
+                PlayerWin = true;
+            }
+            if(PlayerObjectInteraction.JerryPoints == 16)
+            {
+                JerryWin = true;
+            }
         }
     }
 }
